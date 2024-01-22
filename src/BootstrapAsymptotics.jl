578e0778d8b74882589ca19406a7a417627e27c6
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-01-22 12:30:44 +0100

    Mathy part of logistic

--- src/BootstrapAsymptotics.jl
+++ src/BootstrapAsymptotics.jl
@@ -2,10 +2,12 @@ module BootstrapAsymptotics
 
 using IntervalArithmetic: interval
 using LinearAlgebra: Diagonal, Symmetric, I, norm
+using NLSolvers: NLSolvers
 using QuadGK: quadgk
 using Random: AbstractRNG
 using StatsFuns: normpdf, poispdf, logistic
 using StaticArrays: SVector, SMatrix
+using ForwardDiff: derivative
 
 include("utils/problems.jl")
 include("utils/algos.jl")
@@ -15,6 +17,9 @@ include("ridge/simulation.jl")
 include("ridge/state_evolution.jl")
 
 include("logistic/simulation.jl")
+include("logistic/math.jl")
+include("logistic/state_evolution_single.jl")
+include("logistic/state_evolution.jl")
 
 export Ridge, Logistic
 export PairBootstrap, ResidualBootstrap, LabelResampling, FullResampling

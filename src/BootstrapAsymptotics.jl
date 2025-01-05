06be5e35a56dda6cfd0d678c338c57aeee57e60a
lclarte <32338081+lclarte@users.noreply.github.com>
2025-01-05 14:05:42 +0100

    removed ensembledridge for now

--- src/BootstrapAsymptotics.jl
+++ src/BootstrapAsymptotics.jl
@@ -43,7 +43,7 @@ include("state_evolution_logistic.jl")
 include("state_evolution_bayesopt.jl")
 include("state_evolution_kernel.jl")
 include("state_evolution_lasso.jl")
-include("state_evolution_ensemble.jl")
+# include("state_evolution_ensemble.jl") # keep this commented while EnsembledRidge not done 
 
 include("bias_variance.jl")
 include("gamp.jl")
